5d5b4f3ec3aa671b5a58e6af914accc9e31bb5f1
Martin Rios Tineo <68676050+Maxsoll159@users.noreply.github.com>
2024-10-21 17:57:27 -0500

    MonoRepo

---
+++ apps/admission--app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Prueba } from '../../../../packages/components/Prueba';
+
+export default function Home() {
+  return (
+   <div>
+    <Prueba />
+   </div>
+  );
+}
